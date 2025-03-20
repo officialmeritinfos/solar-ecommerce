9af952af046dbacf549982064e43800244de7bfe
Anon Dev <michaelerastus9@gmail.com>
2025-03-20 17:41:32 +0100

    Updated

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CartController extends Controller
+class CartController extends BaseController
 {
-    //
+    //landing page
+    public function index(Request $request)
+    {
+
+    }
 }
